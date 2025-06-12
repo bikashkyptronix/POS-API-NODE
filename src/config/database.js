76efe02e1b28b1757dfb9e69358368539e35465e
bikashkyptronix <bikash.kyptronix@gmail.com>
2025-06-12 15:07:19 +0530

    update the mongo conn

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,10 @@ import mongoose from 'mongoose';
 
 export const connectMongoDB = async () => {
   try {
-    await mongoose.connect(envs.mongodb_uri);
+    await mongoose.connect(envs.mongodb_uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
     console.log("✅ MongoDB connected successfully");
   } catch (error) {
     console.error("❌ MongoDB connection error:", error);
