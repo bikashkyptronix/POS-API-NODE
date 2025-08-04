bb3540afa78e52eb03cf65e868ee142cd0fdcd32
bikashkyptronix <bikash.kyptronix@gmail.com>
2025-08-04 17:04:57 +0530

    file upload for employee task status change

--- src/validations/sites/user/employeeTaskUpdate.js
+++ src/validations/sites/user/employeeTaskUpdate.js
@@ -6,6 +6,7 @@ export const employeeTaskUpdate = celebrate({
     employee_id: Joi.string().trim().required(),
     task_title: Joi.string().trim().required(),
     task_details: Joi.string().required(), // Plain password from request body
-    task_deadline: Joi.date().optional().allow(null, '')
+    task_deadline: Joi.date().optional().allow(null, ''),
+    task_status: Joi.string().valid("pending", "on-going", "defer", "completed").optional(),
   }),
 });
